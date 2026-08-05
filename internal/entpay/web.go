0bdb46905c2ee17a00ef9068428b71e5ebcc5ca1
Eric Ninig <eric.ning@aligntag.com>
2026-08-05 12:44:29 +0800

    fix: scope EntPay favicon URL

--- internal/entpay/web.go
+++ internal/entpay/web.go
@@ -6,6 +6,7 @@ const indexHTML = `<!doctype html>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>EntPay Agent</title>
+  <link rel="icon" href="favicon.ico">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
